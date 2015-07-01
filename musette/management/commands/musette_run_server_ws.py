70876f4fdc92b8e794a31c8b017ec6a4f89918e2
Martin Peveri <martinpeveri@gmail.com>
2015-07-01 16:37:27 -0300

    NoArgsCommand is deprecated since version 1.8 #13

--- musette/management/commands/musette_run_server_ws.py
+++ musette/management/commands/musette_run_server_ws.py
@@ -2,15 +2,16 @@
 import inspect
 import os
 
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "Run server tornado web sockets."
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
+        self.stdout.write('Running server...')
         path = os.path.dirname(
             os.path.abspath(inspect.getfile(inspect.currentframe())))
         path = os.path.dirname(os.path.dirname(path))
-        self.stdout.write('Running server...')
+        self.stdout.write('Run server tornado')
         os.system("python " + path + "/websockets/server.py")
