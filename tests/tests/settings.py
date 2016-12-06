910dffd38f2c99579a52f0db463a13e5788f1f08
Martín Peveri <martinpeveri@gmail.com>
2016-12-06 11:16:44 -0300

    Change config tests

--- tests/tests/settings.py
+++ tests/tests/settings.py
@@ -41,4 +41,7 @@ settings.configure(
     USE_I18N = True,
     USE_L10N = True,
     USE_TZ = True,
+    SITE_NAME = 'Musette Forum',
+    SITE_URL = 'http://localhost:800/',
+    EMAIL_MUSETTE = '',
 )
