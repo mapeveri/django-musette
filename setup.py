5810697b01a1da2f62e008438af57d0a1736739a
Martín Peveri <martinpeveri@gmail.com>
2016-12-07 10:00:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,12 +38,16 @@ setup(
         'django-hitcount==1.2.2'
     ],
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
